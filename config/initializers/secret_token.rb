89d4e2f8977e585f3728b738189fa328e6e28fe8
Crash Aftertouch <crashaftertouch@gmail.com>
2014-05-13 13:03:29 -0400

    Initialize repositoey

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'd45f6f44c40a60cf2016b6774ac7033e0f5e11466a924b864378a0548af7ef34f95f9046b232d0a7710122daac295c160be5ccf48647086622dd3ca2912d48ce'
